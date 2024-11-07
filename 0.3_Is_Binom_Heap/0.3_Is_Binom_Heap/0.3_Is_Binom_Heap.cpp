584ead51c98bf559b863cac336d578c4709bc3f0
Corovinus <apancenok@gmail.com>
2024-11-07 22:25:01 +0300

    Exercises

---
+++ 0.3_Is_Binom_Heap/0.3_Is_Binom_Heap/0.3_Is_Binom_Heap.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <fstream>
+using namespace std;
+int main()
+{
+	ifstream in("input.txt");
+	ofstream out("output.txt");
+	long long n;
+	in >> n;
+	long long count = 0;
+	while (n != 0) {
+		if (n % 2 == 1) {
+			out << count << "\n";
+		}
+		n /= 2;
+		count++;
+	}
+	return 0;
+}
