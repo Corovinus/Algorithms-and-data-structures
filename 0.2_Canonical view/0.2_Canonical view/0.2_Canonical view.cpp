d2f30b9b987d354e9ded5a330af9e35078497064
Corovinus <apancenok@gmail.com>
2024-11-16 01:54:17 +0300

    Init commit

---
+++ 0.2_Canonical view/0.2_Canonical view/0.2_Canonical view.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+#include <fstream>
+using namespace std;
+int main()
+{
+	ifstream in("input.txt");
+	ofstream out("output.txt");
+	int n;
+	in >> n;
+	
+	if (n == 1) {
+		out << 0 << endl;
+		return 0;
+	}
+
+	vector <int> P(n,0);
+	for (int i = 0; i < n; i++)
+	{
+		int x, y;
+		in >> x >> y;
+		P[y - 1] = x; 
+	}
+	for (int i = 0; i < n; i++)
+	{
+		out << P[i] << " ";
+	}
+	return 0;
+}
