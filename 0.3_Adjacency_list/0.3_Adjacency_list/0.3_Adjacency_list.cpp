95e1af87b91aef08bdd3677a14a32c3f39f234ec
Corovinus <apancenok@gmail.com>
2024-11-16 02:03:47 +0300

    Init commit

---
+++ 0.3_Adjacency_list/0.3_Adjacency_list/0.3_Adjacency_list.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+#include <fstream>
+using namespace std;
+int main()
+{
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+
+    int n, m;
+    in >> n >> m;
+
+    vector<vector<int>> List(n);
+    for (int i = 0; i < m; ++i) {
+        int x, y;
+        in >> x >> y;
+        List[x - 1].push_back(y); 
+        List[y - 1].push_back(x); 
+    }
+
+    for (int i = 0; i < n; ++i) {
+        out << List[i].size();
+        for (int x : List[i]) {
+            out << " " << x;  
+        }
+        out << endl;
+    }
+}
